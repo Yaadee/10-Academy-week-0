08893d32b9eafe15e27be93d052a31cb6563a12e
Yaadee <ayyeekoo437@gmail.com>
2024-04-26 15:56:23 +0000

    streamlit done

--- dash.py
+++ dash.py
@@ -1,152 +1,122 @@
+import matplotlib
+matplotlib.use('Agg')
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+# Load the datasets for each city
+Benin_city1_df = pd.read_csv("src/models/datasets/Benin/solar-measurements_benin-malanville_qc.csv", encoding='latin-1', low_memory=False)
+Benin_city2_df = pd.read_csv("src/models/datasets/Benin/solar-measurements_benin-parakou_qc.csv", encoding='latin-1', low_memory=False)
 
+Sirearra_Leone_city1_df = pd.read_csv("src/models/datasets/Sirearra-Leone/solar-measurements_sierraleone-bumbuna_qc.csv", encoding='latin-1', low_memory=False)
+Sirearra_Leone_city2_df = pd.read_csv("src/models/datasets/Sirearra-Leone/solar-measurements_sierraleone-kenema_qc.csv", encoding='latin-1', low_memory=False)
 
-# Load data for each city
-Benin_city1_df = pd.read_csv("src/models/datasets/Benin/solar-measurements_benin-malanville_qc.csv", encoding='latin-1',low_memory=False)
-Benin_city2_df = pd.read_csv("src/models/datasets/Benin/solar-measurements_benin-parakou_qc.csv",encoding='latin-1',low_memory=False)
+Togo_city1_df = pd.read_csv("src/models/datasets/Togo/solar-measurements_togo-dapaong_qc.csv", encoding='latin-1', low_memory=False)
+Togo_city2_df = pd.read_csv("src/models/datasets/Togo/solar-measurements_togo-davie_qc.csv", encoding='latin-1', low_memory=False)
 
-Sirearra_Leone_city1_df = pd.read_csv("src/models/datasets/Sirearra-Leone/solar-measurements_sierraleone-bumbuna_qc.csv", encoding='latin-1',low_memory=False)
-Sirearra_Leone_city2_df = pd.read_csv("src/models/datasets/Sirearra-Leone/solar-measurements_sierraleone-kenema_qc.csv",encoding='latin-1',low_memory=False)
+# Sidebar
+st.sidebar.title('MoonLight Energy Solutions')
+st.sidebar.image("tests/th.jpeg", width=150, use_column_width=False)
+st.sidebar.markdown("""
+    <style>
+        .sidebar-image img {
+            border-radius: 50%;
+            border: 2px solid #ccc;
+            width: 150px;
+            height: 150px;
+            object-fit: cover;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
-Togo_city1_df = pd.read_csv("src/models/datasets/Togo/solar-measurements_togo-dapaong_qc.csv", encoding='latin-1',low_memory=False)
-Togo_city2_df = pd.read_csv("src/models/datasets/Togo/solar-measurements_togo-davie_qc.csv",encoding='latin-1',low_memory=False)
+# Select country
+country_list = ['Benin', 'Sierra Leone', 'Togo']
+selected_country = st.sidebar.selectbox('Select a country', country_list)
 
+# Get the corresponding datasets based on the selected country
+if selected_country == 'Benin':
+    city1_df = Benin_city1_df
+    city2_df = Benin_city2_df
+elif selected_country == 'Sierra Leone':
+    city1_df = Sirearra_Leone_city1_df
+    city2_df = Sirearra_Leone_city2_df
+elif selected_country == 'Togo':
+    city1_df = Togo_city1_df
+    city2_df = Togo_city2_df
 
-st.set_page_config(page_title="MoonLight Energy Solutions",
-                   page_icon=":bar_chart:",
-                   layout="wide"
-                   )
-st.title("MoonLight Energy Solutions")
+# Select plot type
+plot_type_list = ['Histogram', 'Scatter Plot']
+selected_plot_type = st.sidebar.selectbox('Select a plot type', plot_type_list)
 
-@st.cache_data
-def load_data(path:str):
-    data = pd.read_csv("src/models/datasets/Benin/solar-measurements_benin-malanville_qc.csv",encoding='latin-1',low_memory=False)
-    return data
+# Define function to display descriptions
+def display_description(plot_type, city1_df, city2_df):
+    city1_df['GHI'] = pd.to_numeric(city1_df['GHI'], errors='coerce')
+    city2_df['GHI'] = pd.to_numeric(city2_df['GHI'], errors='coerce')
+    city1_df['Tamb'] = pd.to_numeric(city1_df['Tamb'], errors='coerce')
+    city2_df['Tamb'] = pd.to_numeric(city2_df['Tamb'], errors='coerce')
+    
+    if plot_type == 'Histogram':
+        diff1 = city1_df['GHI'].mean() - city2_df['GHI'].mean()
+        diff2 = city1_df['GHI'].median() - city2_df['GHI'].median()
+        return f"In this comparison:\n- City 1's mean GHI is {diff1:.2f} higher than City 2's.\n- City 1's median GHI is {diff2:.2f} higher than City 2's."
+    elif plot_type == 'Scatter Plot':
+        diff1 = city1_df['Tamb'].mean() - city2_df['Tamb'].mean()
+        diff2 = city1_df['Tamb'].median() - city2_df['Tamb'].median()
+        return f"In this comparison of data of:\n- City 1's mean Ambient Temperature is {diff1:.2f} higher than City 2's.\n- City 1's median Ambient Temperature is {diff2:.2f} higher than City 2's."
 
-with st.sidebar:
-    upload_file = st.file_uploader("Choose a file", type=["csv", "xlsx",'pdf'])
+# Create two columns for sidebar and main content
+sidebar, main = st.columns([1, 4])
 
-    if upload_file is None:
-        st.info("upload file through config",icon="ℹ️")
-        st.stop()
+# Sidebar
+with sidebar:
+    # Sidebar content
+    st.sidebar.title('MoonEnergy Solar Installation')
+    # Add sidebar content here
 
-df = load_data(upload_file)
+# Main content
+with main:
+    st.title("Solar Data Analysis")
 
+    # Plot the selected data
+    if selected_plot_type == 'Histogram':
+        fig, axes = plt.subplots(1, 2, figsize=(15, 7))
 
-Benin_city1=Benin_city1_df.describe()
+        # Histograms for City 1
+        axes[0].hist(city1_df['GHI'], bins=20, edgecolor='black')
+        axes[0].set_xlabel('GHI')
+        axes[0].set_ylabel('Frequency')
+        axes[0].set_title(f'Global Horizontal Irradiance (GHI) - City 1')
 
+        # Histograms for City 2
+        axes[1].hist(city2_df['GHI'], bins=20, edgecolor='black')
+        axes[1].set_xlabel('GHI')
+        axes[1].set_ylabel('Frequency')
+        axes[1].set_title(f'Global Horizontal Irradiance (GHI) - City 2')
 
-col1, col2, col3 = st.columns([1,1,1])
+        plt.tight_layout()
+        st.pyplot()
 
+        # Write the description
+        st.write("**Description for Histogram:**")
+        st.write(display_description(selected_plot_type, city1_df, city2_df))
 
-Converted_to_numeric=Benin_city1_df.apply(pd.to_numeric, errors='coerce')
+    elif selected_plot_type == 'Scatter Plot':
+        fig, axes = plt.subplots(1, 2, figsize=(15, 7))
 
-st.write(Converted_to_numeric)
-st.write(Converted_to_numeric.describe())
+        # Scatter plots for City 1
+        axes[0].scatter(city1_df['GHI'], city1_df['Tamb'])
+        axes[0].set_xlabel('GHI')
+        axes[0].set_ylabel('Ambient Temperature (°C)')
+        axes[0].set_title(f'GHI vs. Ambient Temperature - City 1')
 
-st.subheader('Summary Statistics for Benin')
-st.write(Benin_city1_df.describe())
+        # Scatter plots for City 2
+        axes[1].scatter(city2_df['GHI'], city2_df['Tamb'])
+        axes[1].set_xlabel('GHI')
+        axes[1].set_ylabel('Ambient Temperature (°C)')
+        axes[1].set_title(f'GHI vs. Ambient Temperature - City 2')
 
-
-correlation_matrix = benin_city1_df[['GHI', 'DHI', 'DNI', 'TModA', 'TModB']].corr()
-st.subheader('Corrolation matrix')
-st.write(correlation_matrix)
-
-
-Benin_city1_df = Benin_city1_df[Benin_city1_df['Timestamp'] != 'yyyy-mm-dd hh:mm']
-Benin_city1_df['Timestamp'] = pd.to_datetime(Benin_city1_df['Timestamp'])
-
-# Title and Introduction
-st.title('Solar Panel Installation Analysis')
-st.write("Welcome to the solar panel installation analysis blog!")
-
-# Visualization for Benin City
-st.header('Analysis for Benin City')
-
-Benin_city2_df = Benin_city2_df[Benin_city2_df['Timestamp'] != 'yyyy-mm-dd hh:mm']
-Benin_city2_df['Timestamp'] = pd.to_datetime(Benin_city2_df['Timestamp'])
-
-# Title and Introduction
-st.title('Solar Panel Installation Analysis')
-st.write("Welcome to the solar panel installation analysis blog!")
-
-# Visualization for Benin City
-st.header('Analysis for Benin City')
-
-
-
-
-
-# Plot GHI
-plt.figure(figsize=(10, 6))
-plt.plot(Benin_city1_df['Timestamp'].to_numpy(), Benin_city1_df['GHI'], label='GHI', color='blue')
-plt.xlabel('Timestamp')
-plt.ylabel('GHI')
-plt.title('Variation of GHI over Time')
-plt.xticks(rotation=45)
-plt.legend()
-st.pyplot()
-
-# Conclusion
-st.header('Conclusion')
-st.write("In conclusion, the plot above shows the variation of Global Horizontal Irradiance (GHI) over time in Benin City, which is important for understanding the solar radiation pattern.")
-
-
-
-# Visualization: Histogram for GHI (Global Horizontal Irradiance) - City 1
-plt.figure(figsize=(10, 6))
-sns.histplot(Benin_city1_df['GHI'], bins=20, kde=True)
-plt.title('Histogram of GHI in City 1')
-plt.xlabel('GHI')
-plt.ylabel('Frequency')
-st.pyplot()
-
-# Visualization for City 2
-st.header('City 2 Analysis')
-
-# Summary Statistics for City 2
-st.subheader('Summary Statistics for City 2')
-st.write(Benin_city2_df.describe())
-
-# Visualization: Histogram for GHI (Global Horizontal Irradiance) - City 2
-plt.figure(figsize=(10, 6))
-sns.histplot(Benin_city2_df['GHI'], bins=20, kde=True)
-plt.title('Histogram of GHI in City 2')
-plt.xlabel('GHI')
-plt.ylabel('Frequency')
-st.pyplot()
-
-# Comparison between Cities
-st.header('City Comparison')
-
-# Select cities to compare
-selected_cities = st.multiselect('Select cities to compare', ['City 1', 'City 2', 'City 3'])
-
-# Visualization: Histogram for GHI Comparison
-if len(selected_cities) > 1:
-    plt.figure(figsize=(10, 6))
-    for city in selected_cities:
-        if city == 'City 1':
-            sns.histplot(city1_df['GHI'], bins=20, kde=True, label='City 1')
-        elif city == 'City 2':
-            sns.histplot(city2_df['GHI'], bins=20, kde=True, label='City 2')
-        elif city == 'City 3':
-            sns.histplot(city3_df['GHI'], bins=20, kde=True, label='City 3')
-    plt.title('Histogram of GHI Comparison between Cities')
-    plt.xlabel('GHI')
-    plt.ylabel('Frequency')
-    plt.legend()
-    st.pyplot()
-else:
-    st.write("Select at least two cities for comparison")
-
-# Conclusion
-st.header('Conclusion')
-st.write("In conclusion, each city's solar panel installation analysis reveals different levels of solar radiation, which can impact the efficiency and feasibility of solar energy projects.")
-
-# Acknowledgements
-st.write("Thank you for reading our solar panel installation analysis blog!")
-
+        plt.tight_layout()
+        st.pyplot()
+        # Write the description
+        st.write("**Description for Scatter Plot:**")
+        st.write(display_description(selected_plot_type, city1_df, city2_df))
---
+++ stream.py
@@ -0,0 +1,113 @@
+# import streamlit as st
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# # Configure Streamlit page
+# st.set_page_config(page_title="MoonEnergy Solar Installation", page_icon=":sun_with_face:", layout="wide", initial_sidebar_state="expanded")
+
+# # Load the datasets for each city
+# Benin_city1_df = pd.read_csv("src/models/datasets/Benin/solar-measurements_benin-malanville_qc.csv", encoding='latin-1', low_memory=False)
+# Benin_city2_df = pd.read_csv("src/models/datasets/Benin/solar-measurements_benin-parakou_qc.csv", encoding='latin-1', low_memory=False)
+
+# Sirearra_Leone_city1_df = pd.read_csv("src/models/datasets/Sirearra-Leone/solar-measurements_sierraleone-bumbuna_qc.csv", encoding='latin-1', low_memory=False)
+# Sirearra_Leone_city2_df = pd.read_csv("src/models/datasets/Sirearra-Leone/solar-measurements_sierraleone-kenema_qc.csv", encoding='latin-1', low_memory=False)
+
+# Togo_city1_df = pd.read_csv("src/models/datasets/Togo/solar-measurements_togo-dapaong_qc.csv", encoding='latin-1', low_memory=False)
+# Togo_city2_df = pd.read_csv("src/models/datasets/Togo/solar-measurements_togo-davie_qc.csv", encoding='latin-1', low_memory=False)
+
+# # Sidebar
+# page_icon = ":sun_with_face:"
+# st.sidebar.title('MoonLight Energy Solutions')
+# st.sidebar.image("tests/th.jpeg", use_column_width=True)
+
+# # Select country
+# country_list = ['Benin', 'Sierra Leone', 'Togo']
+# selected_country = st.sidebar.selectbox('Select a country', country_list)
+
+# # Get the corresponding datasets based on the selected country
+# if selected_country == 'Benin':
+#     city1_df = Benin_city1_df
+#     city2_df = Benin_city2_df
+# elif selected_country == 'Sierra Leone':
+#     city1_df = Sirearra_Leone_city1_df
+#     city2_df = Sirearra_Leone_city2_df
+# elif selected_country == 'Togo':
+#     city1_df = Togo_city1_df
+#     city2_df = Togo_city2_df
+
+# # Select plot type
+# plot_type_list = ['Histogram', 'Scatter Plot']
+# selected_plot_type = st.sidebar.selectbox('Select a plot type', plot_type_list)
+
+# # Define function to display descriptions
+# def display_description(plot_type, city1_df, city2_df):
+#     city1_df['GHI'] = pd.to_numeric(city1_df['GHI'], errors='coerce')
+#     city2_df['GHI'] = pd.to_numeric(city2_df['GHI'], errors='coerce')
+#     city1_df['Tamb'] = pd.to_numeric(city1_df['Tamb'], errors='coerce')
+#     city2_df['Tamb'] = pd.to_numeric(city2_df['Tamb'], errors='coerce')
+    
+#     if plot_type == 'Histogram':
+#         diff1 = city1_df['GHI'].mean() - city2_df['GHI'].mean()
+#         diff2 = city1_df['GHI'].median() - city2_df['GHI'].median()
+#         return f"In this comparison:\n- City 1's mean GHI is {diff1:.2f} higher than City 2's.\n- City 1's median GHI is {diff2:.2f} higher than City 2's."
+#     elif plot_type == 'Scatter Plot':
+#         diff1 = city1_df['Tamb'].mean() - city2_df['Tamb'].mean()
+#         diff2 = city1_df['Tamb'].median() - city2_df['Tamb'].median()
+#         return f"In this comparison of data of:\n- City 1's mean Ambient Temperature is {diff1:.2f} higher than City 2's.\n- City 1's median Ambient Temperature is {diff2:.2f} higher than City 2's."
+
+# # Create two columns for sidebar and main content
+# sidebar, main = st.columns([1, 4])
+
+# # Sidebar
+# with sidebar:
+#     # Sidebar content
+#     st.sidebar.title('MoonEnergy Solar Installation')
+#     # Add sidebar content here
+
+# # Main content
+# with main:
+#     st.title("Solar Data Analysis")
+
+#     # Plot the selected data
+#     if selected_plot_type == 'Histogram':
+#         fig, axes = plt.subplots(1, 2, figsize=(15, 7))
+
+#         # Histograms for City 1
+#         axes[0].hist(city1_df['GHI'], bins=20, edgecolor='black')
+#         axes[0].set_xlabel('GHI')
+#         axes[0].set_ylabel('Frequency')
+#         axes[0].set_title(f'Global Horizontal Irradiance (GHI) - City 1')
+
+#         # Histograms for City 2
+#         axes[1].hist(city2_df['GHI'], bins=20, edgecolor='black')
+#         axes[1].set_xlabel('GHI')
+#         axes[1].set_ylabel('Frequency')
+#         axes[1].set_title(f'Global Horizontal Irradiance (GHI) - City 2')
+
+#         plt.tight_layout()
+#         st.pyplot()
+
+#         # Write the description
+#         st.write("**Description for Histogram:**")
+#         st.write(display_description(selected_plot_type, city1_df, city2_df))
+
+#     elif selected_plot_type == 'Scatter Plot':
+#         fig, axes = plt.subplots(1, 2, figsize=(15, 7))
+
+#         # Scatter plots for City 1
+#         axes[0].scatter(city1_df['GHI'], city1_df['Tamb'])
+#         axes[0].set_xlabel('GHI')
+#         axes[0].set_ylabel('Ambient Temperature (°C)')
+#         axes[0].set_title(f'GHI vs. Ambient Temperature - City 1')
+
+#         # Scatter plots for City 2
+#         axes[1].scatter(city2_df['GHI'], city2_df['Tamb'])
+#         axes[1].set_xlabel('GHI')
+#         axes[1].set_ylabel('Ambient Temperature (°C)')
+#         axes[1].set_title(f'GHI vs. Ambient Temperature - City 2')
+
+#         plt.tight_layout()
+#         st.pyplot()
+#         # Write the description
+#         st.write("**Description for Scatter Plot:**")
+#         st.write(display_description(selected_plot_type, city1_df, city2_df))
